687d8cc1fd9f05dd2ef77e30b09e02bd57cae015
Karim Adel <87901216+kimoKhanKareem@users.noreply.github.com>
2022-07-05 15:26:35 +0200

    update

--- src/DataContext/DataContext.js
+++ src/DataContext/DataContext.js
@@ -5,22 +5,21 @@ export const DataContext = createContext();
 export const DataProvider = ({ children }) => {
     const [todoName, setTodoName] = useState('');
     const [todos,setTodos] = useState([]);
-
-    useEffect(() => {
-        const todoStore = JSON.parse(localStorage.getItem('todoStore'))
-        if(todoStore) setTodos(todoStore)
-    }, []);
-
-    useEffect(() => {
-        localStorage.setItem('todoStore', JSON.stringify(todos))
-    }, [todos]);
-
-
+    
     const AddTodo = (e)=>{
         e.preventDefault();
         setTodos([...todos, {name:todoName, complete: false}])
         setTodoName('')
     }
+
+    useEffect(() => {
+        localStorage.setItem('todoStore', JSON.stringify(todos))
+    }, [todos]);
+    
+    useEffect(() => {
+        const todoStore = JSON.parse(localStorage.getItem('todoStore'))
+        if(todoStore) setTodos(todoStore)
+    }, []);
 
     return <DataContext.Provider value={{ todoName, setTodoName, todos, setTodos, AddTodo }}>{children}</DataContext.Provider>
     
